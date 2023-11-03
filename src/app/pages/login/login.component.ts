0495a630cfac88f3c90daed7f531f3a1ce1564e5
Yarego Brozek <dev.kiloton@gmail.com>
2023-11-02 23:04:22 -0300

    Creating button login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 export type Tile = {
   color: string;
@@ -6,17 +9,15 @@ export type Tile = {
   rows: number;
   text: string;
   icon: string;
-  route: string
-  isActive: boolean | null
-}
-
+  route: string;
+  isActive: boolean | null;
+};
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
+  imports: [MatCardModule, MatIconModule, MatButtonModule],
 })
-export default class LoginComponent {
-}
+export default class LoginComponent {}
